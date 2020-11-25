eedd958ea61b6149dbf39f941a7285646e05d35a
yigeyouzi <18300702854@163.com>
2020-11-25 13:50:33 +0800

    api

---
+++ api限流/springboot_42/src/main/java/com/itmayiedu/service/OrderService.java
@@ -0,0 +1,15 @@
+package com.itmayiedu.service;
+
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class OrderService {
+
+	public boolean addOrder() {
+		System.out.println("db....正在操作订单表数据库...");
+		return true;
+	}
+
+}
